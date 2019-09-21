f95d149121cfbc824a8a18c4c3bc24c9a2dc1a3d
Anthony <anthony@hevia.dev>
2019-09-21 03:09:43 -0400

    fixing wikipedia API bug

--- api/nlp/testWikiApi.py
+++ api/nlp/testWikiApi.py
@@ -1,37 +1,39 @@
 import wikipediaapi
-# create wiki object with param lang, and as for page by official name
-# spaces in names are underscores
 
 
 wiki_obj = wikipediaapi.Wikipedia('en')
+
+# Takes parameter WikipediaPageSections
+def grab_sections(sections):
+    i = 0
+    ret_sections = {}
+    for s in sections:
+        print(f"We are level {i} and section {s.title}")
+        i += 1  
+        #
+        ret_sections[s.title] = []
+        ret_sections[s.title].append(s.text)
+        grab_sub_sections(ret_sections[s.title], s.sections)
+
+        if i == 3: 
+            return ret_sections
+    return ret_sections
+
+def grab_sub_sections(ret_sections, sub_sections):
+        for l in sub_sections:
+            ret_sections.append(l.text)
+        return
 
 
 
 wiki_page = wiki_obj.page('Shark')
 #check that page exists before requesting info
 if wiki_page.exists():
-    print("Page - Exists: %s" % wiki_page.exists())
+    # print("Page - Exists: %s" % wiki_page.exists())
 
-    print("Page - Title: %s" % wiki_page.title)
+    # print("Page - Title: %s" % wiki_page.title)
 
-    print("Page - Summary: %s" % wiki_page.summary[0:300])
-
-    def print_sections(sections, level=0):
-        for s in sections:
-            if sections == 3:
-                break
-            print("%s\n%s\n\n" % (s.title, s.text))
-            print_sections(s.sections, level + 1)
-
-    print_sections(wiki_page.sections)
-
-    #wiki_html = wikipediaapi.Wikipedia(
-    #    language='cs',
-    #    extract_format=wikipediaapi.ExtractFormat.HTML
-    #)
-
-    page_sharks = wiki_html.page('Sharks')
-    print(wiki_page.text)
-
-    #p_html = wiki_html.page("Sharks")
-    #print(p_html.text)
+    # print("Page - Summary: %s" % wiki_page.summary[0:300])
+    
+    section_map = grab_sections(wiki_page.sections)
+ 
