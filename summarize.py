173387152d247c7b05aa15f35454e285f223bc20
Anthony <anthony@hevia.dev>
2019-09-21 21:27:20 -0400

    deleted unused folder

--- summarize.py
+++ summarize.py
@@ -31,10 +31,24 @@ def summarize_animals(wiki_data, wolfram_data, language="en"):
     return animal_summary
 
 
-def summarize_cities(wiki_data, wolfram_data, language="en"):
+def summerize_cities(wiki_data, wolfram_data, language="en"):
     city_summary = {}
 
+    # Organize and grab the Wikipedia data + making sure the wikipedia data exists
+    if wiki_data['exists']:
+        city_summary['title'] = wiki_data['title']
+        city_summary["summary"] = summerizedSentences(wiki_data["title"], wiki_data["summary"])
+        city_summary['text'] = summerizedSentences(wiki_data["title"], wiki_data["text"])
+        city_summary["related"] = grab_key_phrases(wiki_data["summary"])
+        city_summary['wiki-url'] = wiki_data['wiki-url']
+        city_summary['wiki-success'] = True
+    else:
+        city_summary['wiki-success'] = False
 
+    if not wolfram_data:
+        city_summary["wolfram-success"] = True
+    else:
+        city_summary["wolfram-success"] = False
 
     return city_summary
 
@@ -48,4 +62,3 @@ def summarize_person(wiki_data, wolfram_data, language="en"):
 
     return person_summary
 
-#summerize_animals(create_maps('Shark'))
