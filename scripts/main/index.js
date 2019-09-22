62b9194ea0bfe604e16310726b7571a9f3f88a10
PromAtlas <dylan.ochoa2323@gmail.com>
2019-09-21 22:50:18 -0400

    backend frontend connection

--- scripts/main/index.js
+++ scripts/main/index.js
@@ -1,15 +1,49 @@
 // Homepage for KnowledgeBytes
 
 $(function(){
-
-    $("#searchQuery").on("keypress", function(key){
+    var result, summary;
+    $("#searchQuery").on("keyup ", function(key){
         if (key.which === 13) {
-            query = JSON.stringify({ "query": $(this).val()});
-            window.location.href="lecture.html";
+            ajaxReq(JSON.stringify({ "query": $(this).val()})).done(function(data){
+                result = data;
+                })
+                summary = scrapeResults(result);   
+                console.log(summary);
+                $("#summary").html(summary);
         }
     });
+
     $("#searchSubmit").on('click', function (){
-        query = JSON.stringify({ "query": $(this).val()});
-        window.location.href="lecture.html";
+        ajaxReq(JSON.stringify({ "query": $(this).val()})).done(function(data){
+            result = data;
+            })
+            summary = scrapeResults(result);   
+            console.log(summary);
+            $("#summary").html(summary);
+
+        });
     });
-});
+
+function ajaxReq(data) {
+    
+    return $.ajax({
+        url: "http:localhost:5000/search_query",
+        contentType: 'application/json; charset=UTF-8',
+        type: "POST",
+        data: data,
+        dataType: 'json'
+    });
+}
+
+function scrapeResults(scrape)
+{
+   
+var results = "";
+for (prop in scrape)
+{
+    if(scrape.hasOwnProperty(prop) && scrape[prop])
+        results += "<p>"+prop +": " +  scrape[prop] + "</p>";  
+}``
+return results;
+}
+
